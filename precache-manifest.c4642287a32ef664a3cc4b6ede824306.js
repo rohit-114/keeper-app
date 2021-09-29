df751e69e3dca298fcb1b6d100bc9934095a22be
Rohit <shinderohit004@gmail.com>
2021-09-29 15:20:21 +0530

    Updates

---
+++ precache-manifest.c4642287a32ef664a3cc4b6ede824306.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f7f8e0f748e57d001606828beb0fee9",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "a676678453054d20a144",
+    "url": "/keeper-app/static/js/2.b2c32d4d.chunk.js"
+  },
+  {
+    "revision": "4f0d395752705a3f6f86",
+    "url": "/keeper-app/static/js/main.2613279a.chunk.js"
+  },
+  {
+    "revision": "fb7d45a05bf0dfe0fe92",
+    "url": "/keeper-app/static/js/runtime-main.42e7a143.js"
+  }
+]);
